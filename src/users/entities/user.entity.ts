45ad0a8c95395f114a9704f71d39b1c211521605
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-03-01 12:08:47 +0100

    feat: add swagger

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class User {
+  @ApiProperty()
   id: number;
+  @ApiProperty()
   email: string;
+  @ApiProperty()
   username: string;
+  @ApiProperty()
   password: string;
 }
