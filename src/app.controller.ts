ba2076869f94e4136537d8e8c39d045a4837322a
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-03-04 13:24:05 +0100

    feat: add schema to login

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,27 @@
 import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { ApiBody } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        username: {
+          type: 'string',
+          example: 'john_doe',
+        },
+        password: {
+          type: 'string',
+          example: 'password123',
+        },
+      },
+      required: ['username', 'password'],
+    },
+  })
   @UseGuards(LocalAuthGuard)
   @Post('login')
   login(@Request() req) {
