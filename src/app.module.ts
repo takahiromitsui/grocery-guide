fc2ccbdac6165dcad0544b0f43f1910f649667a2
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-03-04 14:24:45 +0100

    chore: add postgresJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { DrizzleModule } from './drizzle/drizzle.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, DrizzleModule],
   controllers: [AppController],
   providers: [AppService],
 })
