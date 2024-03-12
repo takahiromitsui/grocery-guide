a3eb11a043f36f9ce1336164a92269dccd581a81
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-03-12 16:10:23 +0100

    feat: crate product

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, Request, Logger } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { TsRestHandler, tsRestHandler } from '@ts-rest/nest';
+import { productsContract } from './products.contract';
+import { AuthenticatedGuard } from '../auth/authenticated.guard';
 
-@Controller('products')
+@Controller()
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
+  private readonly logger = new Logger();
+  @TsRestHandler(productsContract)
+  @UseGuards(AuthenticatedGuard)
+  async handler(@Request() req) {
+    return tsRestHandler(productsContract, {
+      create: async ({ body }) => {
+        const sessionData = req.session;
+        const userId = sessionData?.passport?.user?.id;
+        const product = await this.productsService.create(userId, body);
+        if (!product) {
+          return { status: 404, body: null };
+        }
+        return { status: 200, body: product };
+      },
+    });
+  }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { db } from 'db/db';
+import { eq } from 'drizzle-orm';
+import { products, users } from 'db/schema';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  logger = new Logger();
+
+  async create(userId: number, createProductDto: CreateProductDto) {
+    try {
+      const user = await db.query.users.findFirst({
+        where: eq(users.id, userId),
+      });
+      if (!user) {
+        this.logger.error('user not exists');
+        throw new Error('Failed to create product');
+      }
+      const data = await db
+        .insert(products)
+        .values({
+          name: createProductDto.name,
+          imageURL: createProductDto.imageURL,
+          userId: user.id,
+        })
+        .returning({
+          id: products.id,
+          name: products.name,
+          userId: products.userId,
+          createdAt: products.createdAt,
+          updatedAt: products.updatedAt,
+        });
+      if (!data.length) {
+        this.logger.error('callback is empty array');
+        throw new Error('Failed to create product');
+      }
+      this.logger.log('Product created');
+      return data[0];
+    } catch (error) {
+      this.logger.error(error);
+      throw new InternalServerErrorException('Failed to create product');
+    }
+  }
+}
