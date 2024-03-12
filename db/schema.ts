a948cdd9a72794a55b2eebe1d6b57de7cc534e47
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-03-12 14:31:28 +0100

    feat: create products table

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,11 @@
-import { pgTable, serial, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
+import {
+  integer,
+  pgTable,
+  serial,
+  timestamp,
+  varchar,
+} from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
@@ -9,8 +16,28 @@ export const users = pgTable('users', {
   updatedAt: timestamp('updatedAt', { mode: 'string' }).defaultNow(),
 });
 
+export const usersRelations = relations(users, ({ many }) => ({
+  products: many(products),
+}));
+
 export const sessions = pgTable('sessions', {
   sid: varchar('sid', { length: 255 }).primaryKey(),
   sess: varchar('sess', { length: 2048 }).notNull(),
   expire: timestamp('expire').notNull(),
 });
+
+export const products = pgTable('products', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 256 }).notNull(),
+  imageURL: varchar('imageURL', { length: 256 }),
+  createdAt: timestamp('createdAt', { mode: 'string' }).defaultNow(),
+  updatedAt: timestamp('updatedAt', { mode: 'string' }).defaultNow(),
+  userId: integer('userId'),
+});
+
+export const productsRelations = relations(products, ({ one }) => ({
+  author: one(users, {
+    fields: [products.userId],
+    references: [users.id],
+  }),
+}));
