5cac9cb113f201682b5e03203cda59a48272f8b4
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-03-12 11:54:42 +0100

    feat: add logout

--- src/app.contract.ts
+++ src/app.contract.ts
@@ -1,18 +1,31 @@
 import { initContract } from '@ts-rest/core';
 import { LoginSchema } from './users/dto/login-dto';
 import { UserResponseSchema } from './users/users.contract';
+import { z } from 'zod';
 
 const c = initContract();
 
 export const appContract = c.router({
   login: {
     method: 'POST',
-    path: '/',
+    path: '/login',
     responses: {
       201: UserResponseSchema,
     },
     body: LoginSchema,
     summary: 'Login',
     description: 'Login',
+  },
+  logout: {
+    method: 'POST',
+    path: '/logout',
+    responses: {
+      201: z.object({
+        message: z.string(),
+      }),
+    },
+    body: null,
+    summary: 'Logout',
+    description: 'Logout',
   },
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Request,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { TsRestHandler, tsRestHandler } from '@ts-rest/nest';
 import { appContract } from './app.contract';
 import { UsersService } from './users/users.service';
+import { AuthenticatedGuard } from './auth/authenticated.guard';
 
 @Controller()
 export class AppController {
@@ -11,7 +20,7 @@ export class AppController {
     private readonly appService: AppService,
     private readonly userService: UsersService,
   ) {}
-
+  logger = new Logger();
   @TsRestHandler(appContract.login)
   @UseGuards(LocalAuthGuard)
   async login(@Request() req) {
@@ -22,6 +31,22 @@ export class AppController {
         return { status: 404, body: null };
       }
       return { status: 200, body: user };
+    });
+  }
+  @TsRestHandler(appContract.logout)
+  @UseGuards(AuthenticatedGuard)
+  async logout(@Request() req) {
+    return tsRestHandler(appContract.logout, async () => {
+      try {
+        req.session.destroy();
+        this.logger.log('logout successful');
+        return { status: 200, body: { message: 'logout successful' } };
+      } catch (error) {
+        this.logger.error(error);
+        throw new InternalServerErrorException(
+          'Failed to logout. Please try again later.',
+        );
+      }
     });
   }
 }
