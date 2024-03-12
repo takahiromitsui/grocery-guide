30067cb5a62ed1bd02e3f192a32fa1473158d343
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-03-12 14:34:46 +0100

    chore: generate products resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
